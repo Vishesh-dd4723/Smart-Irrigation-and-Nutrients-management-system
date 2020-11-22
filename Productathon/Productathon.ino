ded30d46249addc96e9aad40c9a88ecba15a5f02
Vishesh <bindal.vishesh@gmail.com>
2020-11-22 12:00:39 +0530

    Updated Code:

--- Productathon/Productathon.ino
+++ Productathon/Productathon.ino
@@ -5,17 +5,24 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 2 ,1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-int expected_value = 4;
-int min_value = 2;
-int good_value = 6;
-int moisture = 1;
 bool raining = 0;
 bool will_rain = 0;
-int N,P,K;    // in Kg/ha
-String crop="Rice";
-int crop_N_min,crop_N_max;
-int crop_P_min,crop_P_max;
-int crop_K_min,crop_K_max;
+// Below data is for Cotton crop.
+// Corresponds to 1024
+int expected_value = 700;
+int min_value = 200;
+int good_value = 1000;
+// int moisture = 1;  // From Sensor
+int N = 800;
+int P = 400;
+int K = 400;    // in Kg/ha
+String crop="Cotton";
+int crop_N_min = 600; 
+int crop_N_max = 1000;
+int crop_P_min = 300;
+int crop_P_max = 500;
+int crop_K_min = 250;
+int crop_K_max = 400;
 
 // My Wifi Crediatials, should be changed accordingly.
 char *ssid = "Jai Shri Radhey!";
@@ -30,51 +37,95 @@ String createUrl(int date,int months,int years){
   String place = "Hodal"; 
   int hours = 13;
   String resultUrl = baseUrl + '&' + "q=" + place + '&' + "dt=" + dt_string + '&' + "hour=" + hours;
-  Serial.println("Result Url = " + resultUrl);
+  //Serial.println("Result Url = " + resultUrl);
   return(resultUrl); 
 }
 
 void nutrients_checker(){
+    digitalWrite(D5, 0);   
+    digitalWrite(D6, 0);
+    N = analogRead(A0);  //  Taking value of Soil Moisture, when S1S0 = 00
+    delay(100);
+    digitalWrite(D5, 1);
+    digitalWrite(D6, 0);
+    P = analogRead(A0);  //  Taking value of Soil Moisture, when S1S0 = 01
+    delay(100);
+    digitalWrite(D5, 0);
+    digitalWrite(D6, 1);
+    K = analogRead(A0);  //  Taking value of Soil Moisture, when S1S0 = 10
+    delay(100);
+    lcd.setCursor(0,2);
+    lcd.print("N: OK P: OK K: OK");
    if(N<crop_N_min || P<crop_P_min || K<crop_K_min){   //some nutrients is in less quantity
        String msg="You should use fertilizer that is rich in";
-       if(N<crop_N_min) msg+=" Nitrogen ";
-       if(P<crop_P_min) msg+=" Phosphorus ";
-       if(K<crop_K_min) msg+=" Potassium ";
+       if(N<crop_N_min){
+        msg+=" Nitrogen ";
+        lcd.setCursor(0,2);
+        lcd.print("N: L ");
+       }
+       if(P<crop_P_min){
+        msg+=" Phosphorus ";
+        lcd.setCursor(6,2);
+        lcd.print("P: L ");
+       }
+       if(K<crop_K_min){
+        msg+=" Potassium ";
+        lcd.setCursor(12,2);
+        lcd.print("K: L ");
+       }
        Serial.println(msg);
    }
    else if(N>crop_N_max || P>crop_P_max || K>crop_K_max){
        String msg="Your soil contains high amount of";
-       if(N>crop_N_max) msg+=" Nitrogen ";
-       if(P>crop_P_max) msg+=" Phosphorus ";
-       if(K>crop_K_max) msg+=" Potassium ";
+       if(N>crop_N_max){
+        msg+=" Nitrogen ";
+        lcd.setCursor(0,2);
+        lcd.print("N: H ");
+       }
+       if(P>crop_P_max) {
+        msg+=" Phosphorus ";
+        lcd.setCursor(6,2);
+        lcd.print("P: H ");
+       }
+       if(K>crop_K_max){
+        msg+=" Potassium ";
+        lcd.setCursor(12,2);
+        lcd.print("K: H ");
+       }
        Serial.println(msg);
    }
    else{
        String msg="Your soil is good for growing "+crop;
+       Serial.println(msg);
    }  
 }
 
 void watering(){
-  Serial.println(analogRead(A0));
-  lcd.setCursor(0,0);
-  lcd.print("Raining ");
-  lcd.print(raining);
+  // Taking value of Soil Moisture, when S1S0 = 11
+  digitalWrite(D5, 1);
+  digitalWrite(D6, 1);
+  //Serial.println(analogRead(A0));
   if (raining){
+      lcd.setCursor(0,1);
+      lcd.print("Rain, Sprinkler OFF");
       Serial.println("Its raining!!");
       Serial.println("No need to turn on sprinkler.");
   }
   else if( analogRead(A0) <min_value){
+    lcd.setCursor(0,1);
+    lcd.print("Sprinkler ON       ");
     Serial.println("Emergency!!,Sprinkler should ON");
     while(expected_value > analogRead(A0)){
        digitalWrite(D0,1);
+       break;
      }
     digitalWrite(D0,0);
   }
-  else if(analogRead(A0)<expected_value){
+  else if(analogRead(A0) < expected_value){
     if(will_rain){
        Serial.println("Wait for rain");
        lcd.setCursor(0,1);
-        lcd.print("Sprinklers OFF  ");
+       lcd.print("Sprinklers OFF    ");
     }else{
        Serial.println("No chance of rain,sprinkler should ON");
        while(good_value > analogRead(A0)){
@@ -87,6 +138,8 @@ void watering(){
      }    
    }
  else{
+        lcd.setCursor(0,1);
+        lcd.print("Soil Moisture: GOOD");
         Serial.println("Soil Mosture is good");
      }
 }
@@ -94,6 +147,8 @@ void setup() {
   Wire.begin(D2, D1); 
   lcd.begin(20,4);
   pinMode(D0, OUTPUT);  // Relay 
+  pinMode(D5,OUTPUT); // For controlling MUX Dataline S0
+  pinMode(D6,OUTPUT); // For controlling MUX Dataline S1
   Serial.begin(9600);
   Serial.print("\nConnecting to ");
   Serial.println(ssid);
@@ -111,7 +166,6 @@ void loop()
   if (WiFi.status() == WL_CONNECTED) 
   {
     HTTPClient http; //Object of class HTTPClient
-    //http.begin("http://api.weatherapi.com/v1/forecast.json?key=6138e9fd28844856b87160752202011&q=Hodal&days=2&dt="+"2020-11-23"+"&hour=12");
     http.begin(createUrl(22,11,2020));
     int httpCode = http.GET();
    
@@ -128,15 +182,19 @@ void loop()
       current_day.toLowerCase();
       raining = current_day.indexOf("rain") != -1;
       will_rain = day1 || day2;
-      Serial.println(place);
+      lcd.setCursor(0,0);
+      lcd.print("Place:");
+      lcd.print(place);
+      Serial.println("Place: "+place);
       Serial.println(current_day);
-      Serial.print(day1);
-      Serial.println(day2);
       watering();
+      nutrients_checker();
+      lcd.setCursor(0,3);
+      lcd.print("     By Agri-Venture");
     }
     http.end(); //Close connection
 
   }
   
-  delay(60000);  // 15 sec
+  delay(20000);  // 20 sec
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Code for Productathon 2.0
 
-## See it for making changes in function of NPK and adding LCD commmands if neccessary.
+## Team: Agro-Venture
